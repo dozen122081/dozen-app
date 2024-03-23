605d1ef209991a32aa3b444b2dd9c541ff26e027
dozen122081 <dozenminimul12@gmail.com>
2024-03-23 15:04:43 +0545

    four step

--- src/app/(application)/(personalDashboard)/my-board/_components/ExBoard.tsx
+++ src/app/(application)/(personalDashboard)/my-board/_components/ExBoard.tsx
@@ -14,14 +14,20 @@ const ExBoard = () => {
     return (
         <div className="max-h-screen h-full">
             <Excalidraw>
-            <MainMenu>
-                    <MainMenu.Item onSelect={() => window.alert("Item1")}>
-                        Item1
-                    </MainMenu.Item>
-                    <MainMenu.Item onSelect={() => window.alert("Item2")}>
-                        Item 2
-                    </MainMenu.Item>
-                </MainMenu>
+                <WelcomeScreen>
+                    <WelcomeScreen.Center>
+                        <WelcomeScreen.Center.Logo >DoZen</WelcomeScreen.Center.Logo>
+                        <WelcomeScreen.Center.Heading>
+                            Tap to start Drawing
+                        </WelcomeScreen.Center.Heading>
+                        <WelcomeScreen.Center.Menu>
+                            <WelcomeScreen.Center.MenuItemLink href="https://github.com/excalidraw/excalidraw">
+                                DoZen Board Powered By Excalidraw
+                            </WelcomeScreen.Center.MenuItemLink>
+                            <WelcomeScreen.Center.MenuItemHelp />
+                        </WelcomeScreen.Center.Menu>
+                    </WelcomeScreen.Center>
+                </WelcomeScreen>
             </Excalidraw>
             {/* board comming soon.... */}
             {/* <Excalidraw>
