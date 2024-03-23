72bde0a9b0a222fd68508144aa9eff80af7978ec
dozen122081 <dozenminimul12@gmail.com>
2024-03-23 14:28:42 +0545

    work

---
+++ src/app/(application)/(personalDashboard)/my-board/_components/ExBoard.tsx
@@ -0,0 +1,44 @@
+"use client";
+import {
+    MainMenu,
+    WelcomeScreen
+} from "@excalidraw/excalidraw";
+import dynamic from "next/dynamic";
+const Excalidraw = dynamic(
+    async () => (await import("@excalidraw/excalidraw")).Excalidraw,
+    {
+        ssr: false,
+    },
+);
+const ExBoard = () => {
+    return (
+        <div>
+            <Excalidraw>
+                <WelcomeScreen>
+                    <WelcomeScreen.Center>
+                        <WelcomeScreen.Center.Logo >DoZen</WelcomeScreen.Center.Logo>
+                        <WelcomeScreen.Center.Heading>
+                            Tap to start Drawing
+                        </WelcomeScreen.Center.Heading>
+                        <WelcomeScreen.Center.Menu>
+                            <WelcomeScreen.Center.MenuItemLink href="https://github.com/excalidraw/excalidraw">
+                                DoZen Board Powered By Excalidraw
+                            </WelcomeScreen.Center.MenuItemLink>
+                            {/* <WelcomeScreen.Center.MenuItemHelp /> */}
+                        </WelcomeScreen.Center.Menu>
+                    </WelcomeScreen.Center>
+                </WelcomeScreen>
+                <MainMenu>
+                    <MainMenu.Item onSelect={() => window.alert("Item1")}>
+                        Item1
+                    </MainMenu.Item>
+                    <MainMenu.Item onSelect={() => window.alert("Item2")}>
+                        Item 2
+                    </MainMenu.Item>
+                </MainMenu>
+            </Excalidraw>
+        </div>
+    )
+}
+
+export default ExBoard
