ad2c7259a05438cb9d159d0f6d258fcad0939401
dozen122081 <dozenminimul12@gmail.com>
2024-04-14 10:37:50 +0545

    payment ui

--- src/components/paymentUi/AppPaymentUi.tsx
+++ src/components/paymentUi/AppPaymentUi.tsx
@@ -1,10 +1,22 @@
 "use client"
+import Link from 'next/link'
 import React from 'react'
+import { Button } from '../ui/button'
 
 const AppPaymentUi = () => {
   return (
-    <div className='h-screen w-full'>
-       This is paypal ui!
+    <div className='h-screen w-full flex items-center justify-center'>
+      <div className='flex flex-col gap-4 items-center'>
+        <div className='text-center'>
+          <h2 className='text-2xl'>Please Complete Your Order to continue using DoZen</h2>
+          <p className='text-sm text-muted-foreground'>It's just NPR.2500 for users in Nepal and just $23.25 for users outside Nepal</p>
+        </div>
+        <Link href="/apppayment">
+          <Button>
+            Complete order
+          </Button>
+        </Link>
+      </div>
     </div>
   )
 }
